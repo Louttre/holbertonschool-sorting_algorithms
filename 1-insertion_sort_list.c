0a40094899a2738699f06b077361009212d8b823
root <seth.rueda@gmail.com>
2024-04-10 08:23:53 +0000

    add 1-insertion_sort_list.c

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,43 @@
+#include "sort.h"
+#include <stdio.h>
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *temp = *list, *temp2 = temp->next;
+	listint_t *holder, *holder2;
+	
+	if (!list)
+		return;	
+	while (temp2)
+	{
+		holder2 = temp2->next;
+		while (temp && temp2->n < temp->n)	
+		{
+			holder = temp;
+			temp2->prev = temp->prev;
+			temp->next = temp2->next;
+			temp2->next = holder;
+			temp->prev = temp2;
+			if (temp->next)
+				temp->next->prev = temp;
+			if (temp2->prev)
+				temp2->prev->next = temp2;
+			else
+				*list = temp2;
+			temp = temp2->prev;
+			holder = *list;
+			while (holder)
+			{
+				printf("%d", holder->n);
+				if (!holder->next)
+					printf("\n");
+				else
+					printf(", ");
+				holder = holder->next;
+			}
+		}
+		if (holder2)	
+			temp = holder2->prev;
+		temp2 = holder2;
+	}
+}
